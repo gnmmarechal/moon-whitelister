5742a16c52f945fdd0f946b92742924a933d25b7
Gnm Software 2012 <gnmpolicemata91@gmail.com>
2016-08-06 21:45:24 +0100

    Fixed input?

--- index.lua
+++ index.lua
@@ -3,24 +3,14 @@
 
 -- Version: 1.0
 
--- Function from Sunshell (by Rinnegatamante)
-MAX_RAM_ALLOCATION = 10485760
-
-function copyFile(input,output)
-	inp = io.open(input,FREAD)
+function copyFile(input, output)
+	inp = io.open(input, FREAD)
 	if System.doesFileExist(output) then
 		System.deleteFile(output)
 	end
 	out = io.open(output,FCREATE)
 	size = io.size(inp)
-	index = 0
-	while (index+(MAX_RAM_ALLOCATION/2) < size) do
-		io.write(out,index,io.read(inp,index,MAX_RAM_ALLOCATION/2),(MAX_RAM_ALLOCATION/2))
-		index = index + (MAX_RAM_ALLOCATION/2)
-	end
-	if index < size then
-		io.write(out,index,io.read(inp,index,size-index),(size-index))
-	end
+	io.write(out, 0, io.read(inp, 0, size), size)
 	io.close(inp)
 	io.close(out)
 end
